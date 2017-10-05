2ac130dde9fdea7676dc42557614b45e6bf35349
Floyd Price <f.price@floydprice.com>
2017-10-05 18:25:41 +0100

    more bored css twaeking

--- app/services/Travis.js
+++ app/services/Travis.js
@@ -1,89 +1,93 @@
 var request = require('request'),
-    async = require('async');
+  async = require('async');
+
+var gravatar = require('gravatar');
 
 module.exports = function () {
-    var self = this,
-        requestBuilds = function (callback) {
-            request({
-                'url': 'https://api.' + self.configuration.url + '/repos/' + self.configuration.slug + '/builds?access_token=' + self.configuration.token,
-                'json' : true
-                },
-                function(error, response, body) {
-                    callback(error, body);
-            });
+  var self = this,
+    requestBuilds = function (callback) {
+      request({
+          'url': 'https://api.' + self.configuration.url + '/repos/' + self.configuration.slug + '/builds?access_token=' + self.configuration.token,
+          'json': true
         },
-        requestBuild = function (build, callback) {
-            request({
-                'url': 'https://api.' + self.configuration.url + '/repos/' + self.configuration.slug + '/builds/' + build.id + '?access_token=' + self.configuration.token,
-                'json' : true
-                },
-                function(error, response, body) {
-                  if (error) {
-                    callback(error);
-                    return;
-                  }
-
-                  callback(error, simplifyBuild(body));
-                });
+        function (error, response, body) {
+          callback(error, body);
+        });
+    },
+    requestBuild = function (build, callback) {
+      request({
+          'url': 'https://api.' + self.configuration.url + '/repos/' + self.configuration.slug + '/builds/' + build.id + '?access_token=' + self.configuration.token,
+          'json': true
         },
-        queryBuilds = function (callback) {
-            requestBuilds(function (error, body) {
-                if (error) {
-                  callback(error);
-                  return;
-                }
+        function (error, response, body) {
+          if (error) {
+            callback(error);
+            return;
+          }
 
-                async.map(body, requestBuild, function (error, results) {
-                    callback(error, results);
-                });
-            });
-        },
-        parseDate = function (dateAsString) {
-            return new Date(dateAsString);
-        },
-        getStatus = function (result, state) {
-            if (state === 'started') return "#937731";
-            if (state === 'created') return "#617fc7";
-            if (state === 'canceled') return "#949094";
-            if (result === null || result === 1) return "#993e47";
-            if (result === 0) return "rgba(78, 129, 86, 0.75)";
-
-            return null;
-        },
-        simplifyBuild = function (res) {
-            return {
-                id: self.configuration.slug + '|' + res.number,
-                project: self.configuration.slug,
-                number: res.number,
-                isRunning: res.state === 'started',
-                startedAt: parseDate(res.started_at),
-                finishedAt: parseDate(res.finished_at),
-                requestedFor: res.author_name,
-                status: getStatus(res.result, res.state),
-                statusText: res.state,
-                reason: res.event_type,
-                hasErrors: false,
-                hasWarnings: false,
-                url: 'https://' + self.configuration.url + '/' + self.configuration.slug + '/builds/' + res.id
-            };
-        };
-
-    self.configure = function (config) {
-        self.configuration = config;
-
-        self.configuration.url = self.configuration.url || 'travis-ci.org';
-        self.configuration.token = self.configuration.token || '';
-
-        if (typeof self.configuration.caPath !== 'undefined') {
-            request = request.defaults({
-                agentOptions: {
-                    ca: require('fs').readFileSync(self.configuration.caPath).toString().split("\n\n")
-                }
-            });
+          callback(error, simplifyBuild(body));
+        });
+    },
+    queryBuilds = function (callback) {
+      requestBuilds(function (error, body) {
+        if (error) {
+          callback(error);
+          return;
         }
+
+        async.map(body, requestBuild, function (error, results) {
+          callback(error, results);
+        });
+      });
+    },
+    parseDate = function (dateAsString) {
+      return new Date(dateAsString);
+    },
+    getStatus = function (result, state) {
+      if (state === 'started') return "#937731";
+      if (state === 'created') return "#617fc7";
+      if (state === 'canceled') return "#949094";
+      if (result === null || result === 1) return "#993e47";
+      if (result === 0) return "rgba(78, 129, 86, 0.75)";
+
+      return null;
+    },
+    simplifyBuild = function (res) {
+      return {
+        id: self.configuration.slug + '|' + res.number,
+        project: self.configuration.slug,
+        number: res.number,
+        isRunning: res.state === 'started',
+        startedAt: parseDate(res.started_at),
+        finishedAt: parseDate(res.finished_at),
+        // requestedFor: res.author_name,
+        requestedFor: gravatar.url(res.author_email, {protocol: 'https', s: '100'}),
+        status: getStatus(res.result, res.state),
+        statusText: res.state,
+        reason: res.event_type,
+        hasErrors: false,
+        hasWarnings: false,
+
+        url: 'https://' + self.configuration.url + '/' + self.configuration.slug + '/builds/' + res.id
+      };
     };
 
-    self.check = function (callback) {
-        queryBuilds(callback);
-    };
+  self.configure = function (config) {
+    self.configuration = config;
+
+    self.configuration.url = self.configuration.url || 'travis-ci.org';
+    self.configuration.token = self.configuration.token || '';
+
+    if (typeof self.configuration.caPath !== 'undefined') {
+      request = request.defaults({
+        agentOptions: {
+          ca: require('fs').readFileSync(self.configuration.caPath).toString().split("\n\n")
+        }
+      });
+    }
+  };
+
+  self.check = function (callback) {
+    queryBuilds(callback);
+  };
 };
